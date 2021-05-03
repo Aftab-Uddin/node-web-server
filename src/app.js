b22cf47739b33dda46317a534fcea18863d6591a
Aftab Uddin <aftabuddinsabbir1@gmail.com>
2021-05-03 16:59:03 +0600

    change commit

--- src/app.js
+++ src/app.js
@@ -22,22 +22,22 @@ app.use(express.static(publicDirectoryPath))
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
-        name: 'Andrew Mead'
+        name: 'Aftab Uddin'
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
-        name: 'Andrew Mead'
+        name: 'Aftab Uddin'
     })
 })
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        helpText: 'This is some helpful text.',
+        helpText: 'Search for your help.',
         title: 'Help',
-        name: 'Andrew Mead'
+        name: 'Aftab Uddin'
     })
 })
 
@@ -83,7 +83,7 @@ app.get('/products', (req, res) => {
 app.get('/help/*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Andrew Mead',
+        name: 'Aftab Uddin',
         errorMessage: 'Help article not found.'
     })
 })
@@ -91,7 +91,7 @@ app.get('/help/*', (req, res) => {
 app.get('*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Andrew Mead',
+        name: 'Aftab Uddin',
         errorMessage: 'Page not found.'
     })
 })
